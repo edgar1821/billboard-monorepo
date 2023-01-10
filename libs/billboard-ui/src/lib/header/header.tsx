97f770aa106f0f399bc9781f15ab6a9f41dabc24
Anonymous <edgar.gamarra21@gmail.com>
2023-01-10 14:23:18 -0500

    creacion del componente

--- libs/billboard-ui/src/lib/header/header.tsx
+++ libs/billboard-ui/src/lib/header/header.tsx
@@ -3,7 +3,7 @@ export interface HeaderProps {}
 export function Header(props: HeaderProps) {
   return (
     <header
-      className="bg-white flex text-xs pt-6 pb-6 pr-5 pl-5
+      className="bg-white flex justify-center text-xs pt-6 pb-6 pr-5 pl-5
     h-20ne w-[100%] border-b-2 border-b-[#ededed]"
     >
       <h1>Logo Billboard</h1>
